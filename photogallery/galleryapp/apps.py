18898856af402aa9d548c44c80db70d094f7f663
Ciro <ciromoraismedeiros@gmail.com>
2022-10-27 16:52:54 -0300

    Remove django stuff

--- photogallery/galleryapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GalleryappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'galleryapp'
--- photogallery/galleryapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from .models import Post
-
-def index(request):
-    posts = Post.objects.all()
-    print(posts, len(posts))
-    return render(request, 'galleryapp/feed.html', {'posts': posts})
