dbbf8e6e77f6695920f8aa0a51d8ce44da31fda1
Ciro <ciromoraismedeiros@gmail.com>
2022-10-31 09:20:26 -0300

    Fix: DB populate script not running

--- db/init/2_populate.sql
+++ db/init/2_populate.sql
@@ -1,5 +1,4 @@
-INSERT INTO `posts` (`id`, `user`, `text`, `created`) VALUES
+INSERT INTO posts (id, user, text, created) VALUES
     (1,	'ciro',	'this is a test', '2022-10-29 17:47:59'),
     (2,	'jesus', 'a second post', '2022-10-31 11:04:02'),
-    (3,	'satan', 'a third post', '2022-10-31 13:25:47'),
-;
+    (3,	'satan', 'a third post', '2022-10-31 13:25:47');
