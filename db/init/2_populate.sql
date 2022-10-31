7e5c059932a3afe34a549a1aaa3c68b176743640
Ciro <ciromoraismedeiros@gmail.com>
2022-10-31 08:51:57 -0300

    Populate database on initialization

---
+++ db/init/2_populate.sql
@@ -0,0 +1,5 @@
+INSERT INTO `posts` (`id`, `user`, `text`, `created`) VALUES
+    (1,	'ciro',	'this is a test', '2022-10-29 17:47:59'),
+    (2,	'jesus', 'a second post', '2022-10-31 11:04:02'),
+    (3,	'satan', 'a third post', '2022-10-31 13:25:47'),
+;
