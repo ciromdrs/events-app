5f6103205105936b57ab89659a6a1117417e2f7b
Ciro <ciromoraismedeiros@gmail.com>
2022-11-01 15:13:55 -0300

    Fix: set current time default to post creation date in database schema

--- db/init/1_schema.sql
+++ db/init/1_schema.sql
@@ -1,5 +1,3 @@
--- Adminer 4.8.1 MySQL 8.0.31 dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -11,9 +9,6 @@ CREATE TABLE `posts` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `user` varchar(100) NOT NULL,
   `text` varchar(1000) NOT NULL DEFAULT '',
-  `created` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- 2022-10-29 17:35:08
