42a6eb40091ead224f4e9f75c9cc3593088de2e4
Ciro <ciromoraismedeiros@gmail.com>
2022-11-03 14:55:06 -0300

    Use base URI for PostsTest

--- php/tests/PostsTest.php
+++ php/tests/PostsTest.php
@@ -4,9 +4,14 @@ require_once("RESTTestCase.php");
 
 
 final class PostsTest extends RESTTestCase {
+    function __construct() {
+        parent::__construct(['base_uri' => 'elm-photo-gallery-site-1/api/']);
+    }
+
+
     function testStartsEmpty(): void {
-        $response = $this->client->get("elm-photo-gallery-site-1/api/posts");
+        $response = $this->client->get('posts');
         $got = (string) $response->getBody();
-        $this->assertEquals("[]", $got);
+        $this->assertEquals('[]', $got);
     }
 }
