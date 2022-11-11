d7ff85d95b160daf1cb6243f1ea201a7813f379e
Ciro <ciromoraismedeiros@gmail.com>
2022-11-11 18:31:33 -0300

    Remove useless routes from routes.php

--- php/website/routes.php
+++ php/website/routes.php
@@ -2,17 +2,6 @@
 
 require_once __DIR__.'/router.php';
 
-// ##################################################
-// ##################################################
-// ##################################################
-
-// Static GET
-// In the URL -> http://localhost
-// The output -> Index
-get('/', 'website/index.html');
-
-get('/static/*', 'website/static/');
-
 get('/api/posts', 'api/posts.php');
 get('/api/posts/$id', 'api/posts.php');
 post('/api/posts', 'api/posts.php');
