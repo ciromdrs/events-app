618c360f9b75bb63ff7c8ad0597dd2ef84465cac
Ciro <ciromoraismedeiros@gmail.com>
2022-11-21 17:07:29 -0300

    Create events table (no relation with posts yet)

--- db/init/1_schema.sql
+++ db/init/1_schema.sql
@@ -5,6 +5,15 @@ SET foreign_key_checks = 0;
 SET NAMES utf8mb4;
 
 
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `owner` varchar(100) NOT NULL,
+    `name` varchar(100) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE `images` (
     `id` int unsigned NOT NULL AUTO_INCREMENT,
